498e5c3cea3737d56b7a21c9a828690cbfab5404
devjparker <95291711+devjparker@users.noreply.github.com>
2022-02-06 10:01:47 -0600

    My variation of the 3n+1
    Typed this out before reading my textbook's solution. This is good practice as Java loops and control structures are just being introduced.

---
+++ 3n+1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class PlayAround {
+	
+	public static void main(String[] args) {
+		
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter a positive integer");
+		int N = obj.nextInt();
+		while (N > 1) {
+			if (N % 2 == 0) {
+				N = N / 2;
+				System.out.println(N);
+			}
+			else {
+				N = (3 * N) + 1;
+				System.out.println(N);
+			}
+		}
+	}
+}
