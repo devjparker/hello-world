546919f545ef972afd1264f876b29cfa84e8f901
devjparker <95291711+devjparker@users.noreply.github.com>
2022-02-06 10:03:43 -0600

    Update 3nadd1.java
    Adding note to let me know to add another loop later for non-positive integers.

--- 3nadd1.java
+++ 3nadd1.java
@@ -6,7 +6,7 @@ public class PlayAround {
 	public static void main(String[] args) {
 		
 		Scanner obj = new Scanner(System.in);
-		System.out.println("Enter a positive integer");
+		System.out.println("Enter a positive integer"); //I'll need to add another loop for if they put a non-positive integer. 
 		int N = obj.nextInt();
 		while (N > 1) {
 			if (N % 2 == 0) {
