83424d5c4956a5fc0f6f289db4f5e3d385a1f8e5
devjparker <95291711+devjparker@users.noreply.github.com>
2022-02-08 16:37:16 -0600

    Update textbookexercises.java

--- textbookexercises.java
+++ textbookexercises.java
@@ -6,3 +6,15 @@ public class MyClass {
       System.out.println("Your die roll is " + roll);
     }
 }
+
+//Program that says hello with inserted name. Will need to test on own computer bc it's acting silly with online compiler.
+import javax.swing.JOptionPane;
+public class MyClass {
+    public static void main(String args[]) {
+      String intro = "What is your name?";
+      String name = JOptionPane.showInputDialog(hello);
+      name = name.toUpperCase();
+      String hello = "Hello" + name + "It's nice to meet you.";
+      JOptionPane.showMessageDialog(hello);
+    }
+}
